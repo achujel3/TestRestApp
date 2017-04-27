e8a11900d8e723856cc1143590ab078557f7d87f
achujelas123 <achujel3@gmail.com>
2017-04-27 16:12:47 +0300

    testing annotation params and other types of parameter setting

---
+++ src/main/java/TestRestApp/resource/InjectDemoResource.java
@@ -0,0 +1,27 @@
+package TestRestApp.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/injectdemo")
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+
+    @GET
+    @Path("annotations")
+    public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
+                                            @HeaderParam("header") String headerParam,
+                                            @CookieParam("cookieParam") String cookieParam) {
+        return "Matrix param: " + matrixParam + "\n" +
+                "Header param: " + headerParam + "\n" +
+                "Cookie param: " + cookieParam;
+    }
+
+}
--- src/main/java/TestRestApp/service/MessageService.java
+++ src/main/java/TestRestApp/service/MessageService.java
@@ -4,6 +4,7 @@ import TestRestApp.database.DatabaseClass;
 import TestRestApp.model.Message;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -11,13 +12,33 @@ public class MessageService {
 
     private Map<Long, Message> messages = DatabaseClass.getMessages();
 
-    public MessageService () {
+    public MessageService() {
         messages.put(1L, new Message(1, "MessageText1", "Tom"));
         messages.put(2L, new Message(2, "MessageText2", "Dom"));
     }
 
     public List<Message> getAllMessages() {
         return new ArrayList<Message>(messages.values());
+    }
+
+    public List<Message> getAllMessagesForYear(int year) {
+        List<Message> messagesForYear = new ArrayList<Message>();
+        Calendar calendar = Calendar.getInstance();
+        for (Message message : messages.values()) {
+            calendar.setTime(message.getCreated());
+            if (calendar.get(Calendar.YEAR) == year) {
+                messagesForYear.add(message);
+            }
+        }
+        return messagesForYear;
+    }
+
+    public List<Message> getAllMessagesPaginated(int start, int size) {
+        ArrayList<Message> list = new ArrayList<Message>(messages.values());
+        if (start + size > list.size()) {
+            return new ArrayList<Message>();
+        }
+        return list.subList(start, start + size);
     }
 
     public Message getMessage(long id) {
