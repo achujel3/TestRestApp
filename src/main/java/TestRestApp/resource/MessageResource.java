271e125546ec317ff9a2d733fc3a543b381b6b0b
achujelas123 <achujel3@gmail.com>
2017-04-27 13:32:10 +0300

    testing CRUD

--- src/main/java/TestRestApp/resource/MessageResource.java
+++ src/main/java/TestRestApp/resource/MessageResource.java
@@ -3,7 +3,11 @@ package TestRestApp.resource;
 import TestRestApp.model.Message;
 import TestRestApp.service.MessageService;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,21 +15,39 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/messages")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
+
+    @GET
+    @Path("/{messageId}")
+    public Message getMessage(@PathParam("messageId") long id) {
+        return messageService.getMessage(id);
+    }
 
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
 
-    @GET
+    @POST
+    public Message addMessage(Message message) {
+        return messageService.addMessage(message);
+    }
+
+    @PUT
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Message getMessage(@PathParam("messageId") long id) {
-        return messageService.getMessage(id);
+    public Message removeMessage(@PathParam("messageId") long id, Message message) {
+        message.setId(id);
+        return messageService.updateMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}")
+    public void updateMessage(@PathParam("messageId") long id, Message message) {
+        messageService.removeMessage(id);
     }
 
 
